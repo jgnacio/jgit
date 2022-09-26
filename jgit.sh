14b765b07378431c70d41824fbf8c3d3580f2a40
jgnacio <jgnaciogomez@gmail.com>
2022-09-26 12:00:34 -0700

    add jgit.sh

--- jgit.sh
+++ jgit.sh
@@ -1,7 +1,7 @@
 #/bin/bash
-read -p "files to send: " archives_to_push
+read -e -p "files to send: " archives_to_push
 git add $archives_to_push
-read -p "message fot this commit: " message
+read -e -p "message fot this commit: " message
 git commit -m "$message"
 git status
 git push
