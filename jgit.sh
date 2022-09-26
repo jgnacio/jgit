f2b284605dbacb60c5e4784bf96a20579591ce7d
jgnacio <jgnaciogomez@gmail.com>
2022-09-26 12:08:22 -0700

    add jgit.sh

--- jgit.sh
+++ jgit.sh
@@ -1,7 +1,13 @@
 #/bin/bash
+git fetch
+git pull
 read -e -p "files to send: " archives_to_push
 git add $archives_to_push
 read -e -p "message fot this commit: " message
 git commit -m "$message"
+read -e -p "Your user name of github for config" user_name
+git config user.name "$user_name"
+read -e -p "Your email of github for config" user_email
+git config user.email "$user_email"
 git status
 git push
