9cc083a7364c0b1321969576d4ddabb41f6774ff
jgnacio <jgnaciogomez@gmail.com>
2022-09-26 12:32:15 -0700

    add jgit.sh

--- jgit.sh
+++ jgit.sh
@@ -9,11 +9,13 @@ git commit -m "$message"
 user_name="git config user.name"
 user_email="git config user.email"
 
-if [[ -z $user_name ]];then
+if [[ -z $user_name ]]
+then
 	echo 'user.name is not set'
 	read -e -p "Your user name of github for config" $user_name
 	git config user.name "$user_name"
-if [[ -z $user_email ]];then
+if [[ -z $user_email ]]
+then
 	echo 'user.email is not set'
 	read -e -p "Your email of github for config" $user_email
 	git config user.email "$user_email"
