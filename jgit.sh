c1c172c7853e8404ed148b8d579b368e7d38ef19
jgnacio <jgnaciogomez@gmail.com>
2022-09-26 12:37:46 -0700

    update

--- jgit.sh
+++ jgit.sh
@@ -9,18 +9,25 @@ git commit -m "$message"
 user_name="git config user.name"
 user_email="git config user.email"
 
-if [[ -z $user_name ]]
-then
+if [[ -z $user_name ]];then
 	echo 'user.name is not set'
 	read -e -p "Your user name of github for config" $user_name
 	git config user.name "$user_name"
 fi
-if [[ -z $user_email ]]
-then
+if [[ -z $user_email ]];then
 	echo 'user.email is not set'
 	read -e -p "Your email of github for config" $user_email
 	git config user.email "$user_email"
 fi
+
+response="y"
+
+read -e -p "You are sure to push this files?[Y/n]: " $response
+
+if [[ $response -eq "n" ]];then
+	set -e
+fi
+
 git status
 git push
 
